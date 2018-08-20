88aff6b7aeb81d602e5d7279750ee15d3fb79106
norrard148 <norrard@likelion.org>
2018-08-21 02:53:48 +0900

    1th

--- db/migrate/20180811122119_create_posts.rb
+++ db/migrate/20180811122119_create_posts.rb
@@ -6,10 +6,6 @@ class CreatePosts < ActiveRecord::Migration[5.1]
       t.integer :author_id
       t.string :time
       t.integer :in
-<<<<<<< HEAD
-=======
-
->>>>>>> e636cc649b376cafd046c86b26b63174c9c46611
       t.timestamps
     end
   end
