0c48476f7006fc0ad668907b3834af9324e2fe9b
norrard148 <norrard@likelion.org>
2018-08-23 09:59:19 +0900

    50th

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,24 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :go]
+  respond_to :html, :js
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
-  
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  def go
+    
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    
   end
 
   # GET /posts/new
@@ -61,6 +69,8 @@ class PostsController < ApplicationController
       end
     end
   end
+
+
 
   # DELETE /posts/1
   # DELETE /posts/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -14,21 +14,24 @@ Rails.application.routes.draw do
   get 'education/contraception'
 
   devise_for :users
+  get 'posts/go/:id', to: 'posts#go', as: 'posts_g'
   resources :posts
   get 'home/index'
-
+  
   get 'home/education'
 
   get 'home/quiz'
 
   get 'user/index'
-
-
+  
   root "home#index"
+ 
     resources :posts do
+     
       resources :comments, only: [:create, :destroy]
+     
     end
-
+    
   get 'counsel/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
