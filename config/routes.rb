701c496ccc2656c223c5558f824f742d123683fc
norrard148 <norrard@likelion.org>
2018-08-24 01:31:51 +0900

    56th

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get 'education/contraception'
   get 'education/go'
   get 'education/go1'
+  get 'education/go2'
+  get 'education/go3'
   devise_for :users
   get 'posts/go/:id', to: 'posts#go', as: 'posts_g'
   resources :posts
