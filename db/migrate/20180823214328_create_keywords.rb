cb3f80773ac3928664dfe30b9c14d00802cb984b
norrard148 <norrard@likelion.org>
2018-08-25 01:35:19 +0900

    dd

--- db/migrate/20180823214328_create_keywords.rb
+++ db/migrate/20180823214328_create_keywords.rb
@@ -2,7 +2,7 @@ class CreateKeywords < ActiveRecord::Migration[5.1]
   def change
     create_table :keywords do |t|
       t.string :keyword
-
+      t.integer :number
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180823214328) do
 
   create_table "keywords", force: :cascade do |t|
     t.string "keyword"
+    t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
